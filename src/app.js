e06b2413ee1a867f579ec2b07148496ac142993d
yannam-hema <yannamhema30@gmail.com>
2025-10-14 21:35:31 +0530

    Starting with API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express=require("express");
 const app=express();
+
+app.get("/ab+c",(req,res)=>{
+    res.send("abc page");
+})
+
+app.get("/user/",(req,res)=>{
+    res.send("user information git successfully !");
+})
+
+app.post("/user",(req,res)=>{
+    res.send("user information post successfully !")
+})
+
+app.use("/",(req,res)=>{
+    res.send("hello world !");
+})
+
 app.use("/hema",(req,res)=>{
     res.send("hello Hema ! ")
 })
+
 app.listen(3000,()=>{
     console.log("server is running ....");
 })
